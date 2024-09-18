08c5d51b98273f814b2e3a5a0c5d36d3c8cbeafe
Rysbai Amanbai <rysbai.a@jetbase.io>
2024-09-18 17:40:00 +0600

    2024-09-18

--- dxf_parser.py
+++ dxf_parser.py
@@ -1,6 +1,7 @@
 import argparse
 import time
 import re
+import itertools
 from collections import namedtuple, OrderedDict
 from decimal import Decimal
 import logging
@@ -13,23 +14,40 @@ _log.setLevel(logging.DEBUG)
 # 3 decimal digits
 FLOAT_TOLLERENCE = 3
 ENTITY_TYPE = "  0"
-class Point(namedtuple("P", ("x", "y", "z", "id"), defaults=(None, ))):
+
+DOF_MAP = {
+    "x": 1,
+    "y": 2,
+    "z": 3,
+    "fx": 4,
+    "fy": 5,
+    "fz": 6,
+}
+
+
+class Point(namedtuple("P", ("x", "y", "z", "id"), defaults=(None,))):
     pass
 
 
-class Layer(namedtuple("LAYER", ("name", "id", "attrs", "lines", "faces", "points", "lwpolines"))):
+class Layer(
+    namedtuple(
+        "LAYER",
+        ("name", "id", "type", "attrs", "lines", "faces", "points", "lwpolines"),
+    )
+):
     pass
 
 
 Line = namedtuple("LINE", ("a", "b"))
-ThreeDFace = namedtuple("ThreeDFace", ('points'))
-LwPolyLine = namedtuple("LWPOLYLINE", ('points', ))
+ThreeDFace = namedtuple("ThreeDFace", ("points"))
+LwPolyLine = namedtuple("LWPOLYLINE", ("points",))
 
 
 POINTS: dict[tuple[str, str, str], Point] = OrderedDict()
 SEQUENCES = {}
 
-def _counter(sequence_id: str, start = 1, step = 1):
+
+def _counter(sequence_id: str, start=1, step=1):
     SEQUENCES[sequence_id] = start
 
     while True:
@@ -37,12 +55,12 @@ def _counter(sequence_id: str, start = 1, step = 1):
         SEQUENCES[sequence_id] = SEQUENCES[sequence_id] + step
 
 
-POINT_ID_SEQUENCE = _counter('points')
-LAYER_ID_SEQUENCE = _counter('layers')
+POINT_ID_SEQUENCE = _counter("points")
+LAYER_ID_SEQUENCE = _counter("layers")
 
 
 def strip_tollerence(num: str) -> str:
-    f = num.split('.')
+    f = num.split(".")
     if len(f) == 2:
         return f[0] + "." + f[1][:FLOAT_TOLLERENCE]
 
@@ -70,7 +88,7 @@ def read_code_value(f: TextIO, code: str, parser: Callable = str) -> str:
         if line == "":  # end of file
             return "ENDOFFILE"
 
-        if line == (code + '\n'):
+        if line == (code + "\n"):
             return parser(f.readline().strip())
 
 
@@ -118,12 +136,12 @@ def parse_LWPOLYLINE(f: TextIO) -> LwPolyLine:
         if line == "":  # end of file
             break
 
-        if line == '  0':  # end of the section or entity
+        if line == "  0":  # end of the section or entity
             break
 
         x = line.strip()
-        y = read_code_value(f, ' 20')
-        z = '0.00'
+        y = read_code_value(f, " 20")
+        z = "0.00"
 
         points.add(Point(point_factory(x, y, z)))
 
@@ -144,26 +162,67 @@ ENTITY_MAPS = {
     "LWPOLYLINE": "lwpolylines",
 }
 
+DOF_VALUES = ["x", "y", "z", "fx", "fy", "fz"]
+
+
+def remove_invalid_layer(layer_name: str, layers: dict[str, Layer]):
+    layer = layers.pop(layer_name, None)
+    if not layer:
+        return
+
+    # Reset point id sequences
+    if not SEQUENCES.get('points'):
+        return
+    
+    SEQUENCES['points'] = SEQUENCES['points'] - (len(layer.lines) * 2)
+    SEQUENCES['points'] = SEQUENCES['points'] - len(layer.points)
+    SEQUENCES['points'] = SEQUENCES['points'] - len(list(itertools.chain.from_iterable(layer.faces)))
+
 
 def parse_layer_name(entity_type: str, layer_name: str) -> list[str]:
-    b_pattern = r'B\d+'
-    h_pattern = r'H\d+'
+    b_pattern = r"B\d+"
+    h_pattern = r"H\d+"
 
-    if entity_type == 'LINE':
+    if entity_type == "LINE":
         b_val = re.findall(b_pattern, layer_name)
         h_val = re.findall(h_pattern, layer_name)
 
         if not (b_val and h_val):
+            _log.error(
+                f"Invalid layer name for 3DFACE: {layer_name}"
+                "\nB and H is not provided"
+            )
             return []
 
         return [float(b_val[0][1:]), float(h_val[0][1:])]
 
-    if entity_type == '3DFACE':
+    if entity_type == "3DFACE":
         h_val = re.findall(h_pattern, layer_name)
         if not h_val:
+            _log.error(
+                f"Invalid layer name for 3DFACE: {layer_name}" "\nH is not provided"
+            )
             return []
 
         return [float(h_val[0][1:]) / 100]
+
+    if entity_type == "POINT":
+        splits = layer_name.split("DOF ")
+        if len(splits) < 2:
+            _log.error(f"Invalid layer name for POINT: {layer_name}")
+            return []
+
+        dofs = splits[1].lower()
+        dofs = [dof.strip() for dof in dofs.split(" ") if dof.strip() in DOF_VALUES]
+        lira_dofs = [str(DOF_MAP[d]) for d in dofs]
+        if not all(lira_dofs[i] <= lira_dofs[i + 1] for i in range(len(lira_dofs) - 1)):
+            _log.error(
+                f"Invalid layer name for POINT: {layer_name}" "DOFS are not sorted"
+            )
+            return []
+
+        print(f"{layer_name=} VALID!")
+        return dofs
 
 
 def parse_entities(f: TextIO):
@@ -184,14 +243,29 @@ def parse_entities(f: TextIO):
             _log.warning(f"Parser for {entity_type} not found")
             continue
 
-        layer_name = read_code_value(f, '  8')
-        attrs = parse_layer_name(entity_type, layer_name)
-        if not attrs:  # Invalid layer name
-            continue
-    
+        layer_name = read_code_value(f, "  8")
+
         layer = layers.get(layer_name)
+        if layer and layer.type != entity_type:
+            _log.error(f'Layer={layer_name} contains multiple type of entities')
+            remove_invalid_layer(layer_name, layers)
+            continue
+
         if not layer:
-            layer = Layer(layer_name, next(LAYER_ID_SEQUENCE), attrs, [], [], [], [])
+            attrs = parse_layer_name(entity_type, layer_name)
+            if not attrs:  # Invalid layer name
+                continue
+
+            layer = Layer(
+                layer_name,
+                next(LAYER_ID_SEQUENCE),
+                entity_type,
+                attrs,
+                [],
+                [],
+                [],
+                [],
+            )
             layers[layer_name] = layer
 
         entity = entity_parser(f)
@@ -203,7 +277,7 @@ def parse_entities(f: TextIO):
 def parse_dxf(filename: str):
     start = time.time()
     layers = []
-    result_fname, _ = filename.split('.')
+    result_fname, _ = filename.split(".")
     with open(filename) as f:
         while True:
             section = read_code_value(f, "  2")
@@ -215,9 +289,10 @@ def parse_dxf(filename: str):
                 layers = parse_entities(f)
 
     end = time.time()
-    _log.warning(f'Finished parsing in: {end - start:.4f} seconds')
+    _log.warning(f"Finished parsing in: {end - start:.4f} seconds")
 
     from lira_exporter import export_to_lira_csv
+
     export_to_lira_csv(layers, POINTS.values(), result_fname)
 
     # with open(result_fname + '_result.json', 'w') as f:
@@ -225,23 +300,23 @@ def parse_dxf(filename: str):
     #     json.dump(result, f)
 
     end = time.time()
-    _log.warning(f'Finished writing results and whole process in: {end - start:.4f} seconds')
+    _log.warning(
+        f"Finished writing results and whole process in: {end - start:.4f} seconds"
+    )
 
     return layers
 
 
 def _dump(obj: tuple | list[tuple]) -> Any:
-    if isinstance(obj, tuple) and hasattr(obj, '_asdict'):
-        return {
-            obj.__class__.__name__: {k: _dump(v) for k, v in obj._asdict().items()}
-        }
+    if isinstance(obj, tuple) and hasattr(obj, "_asdict"):
+        return {obj.__class__.__name__: {k: _dump(v) for k, v in obj._asdict().items()}}
 
     if isinstance(obj, (list, set)):
         return [_dump(el) for el in obj]
 
     if isinstance(obj, Decimal):
         return str(obj)
- 
+
     return obj
 
 
@@ -249,5 +324,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--filename", default="test-files/Drawing1.dxf", required=False)
     args = parser.parse_args()
-    _log.warning(f"Parsing file: {args.filename}", )
+    _log.warning(
+        f"Parsing file: {args.filename}",
+    )
     parse_dxf(args.filename)
--- lira_exporter.py
+++ lira_exporter.py
@@ -1,16 +1,24 @@
 
+DOF_MAP = {
+    'x': 1,
+    'y': 2,
+    'z': 3,
+    'fx': 4,
+    'fy': 5,
+    'fz': 6,
+}
 
-def unique_points(face):
-    points = []
-    ids = set()
-    for point in points:
-        if point.id in ids:
-            continue
+SEQUENCES = {}
 
-        points.append(point)
-        ids.add(point.id)
+def _counter(sequence_id: str, start=1, step=1):
+    SEQUENCES[sequence_id] = start
 
-    return points
+    while True:
+        yield SEQUENCES[sequence_id]
+        SEQUENCES[sequence_id] = SEQUENCES[sequence_id] + step
+
+
+POINT_ID_SEQUENCE = _counter("points")
 
 
 def _write_to_lira_file(f, layers, points):
@@ -40,6 +48,9 @@ def _write_to_lira_file(f, layers, points):
     f.writelines(['\n', ')(3/\n'])
     lines = []
     for layer in layers:
+        if len(layer.points) > 0:
+            continue
+
         attrs = " ".join([str(at) for at in layer.attrs])
         line = f'{layer.id} S0 3.06E6 {attrs}/\n' # For LINE
         if len(layer.attrs) == 1:  # 3DFACE
@@ -53,6 +64,21 @@ def _write_to_lira_file(f, layers, points):
     f.writelines(['\n', ')(4/\n',])
     for point in points:
         f.writelines([f'{point.x} {point.y} {point.z}/\n'])
+    
+
+    # DOFS
+    f.writelines(['\n', ')(5/\n'])
+    for layer in layers:
+        if not len(layer.points):
+            continue
+
+        lines = []
+        for point in layer.points:
+            dofs = ' '.join([str(DOF_MAP[d]) for d in layer.attrs])
+            lines.append(f'{point.id} ' + dofs + '/\n')
+
+        f.writelines(lines)
+
 
     # What is these?
     f.writelines([
